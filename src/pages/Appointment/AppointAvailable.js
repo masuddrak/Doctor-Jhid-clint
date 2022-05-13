2058d42cae57df12731b0606ac4c60018a0c6d73
masud rana <masudranaacb@gmail.com>
2022-05-13 21:44:08 +0600

    added modal appoint

--- src/pages/Appointment/AppointAvailable.js
+++ src/pages/Appointment/AppointAvailable.js
@@ -1,9 +1,11 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import BookingModal from './BookingModal';
 import Service from './Service';
 
 const AppointAvailable = ({ date }) => {
     const [services, setServices] = useState([])
+    const [treatment,setTreatment]=useState(null)
     useEffect(() => {
         fetch('services.json')
             .then(res => res.json())
@@ -18,9 +20,13 @@ const AppointAvailable = ({ date }) => {
                         services.map(service=><Service
                         key={service._id}
                         service={service}
+                        setTreatment={setTreatment}
                         ></Service>)
                     }
                 </div>
+                {
+                    treatment && <BookingModal treatment={treatment}></BookingModal>
+                }
             </div>
         </div>
     );
--- src/pages/Appointment/Service.js
+++ src/pages/Appointment/Service.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import Mybtn from '../sheard/Mybtn';
 
-const Service = ({ service }) => {
+const Service = ({ service, setTreatment }) => {
     console.log(service)
-    const { name,slots } = service
+    const { name, slots } = service
+
     return (
         <div class="card  bg-base-100 shadow-xl ">
             <div class="card-body">
                 <h2 class="card-title text-secondary">{name}</h2>
-                <p>{slots.length>0? <span>{slots[0]}</span>:<span className='text-red-500'>No Slot Available</span>}</p>
-                <p className=' '>{slots.length} {slots.length>1?'SPACES':'SPACE'} AVAILABLE</p>
-                <div  class="card-actions justify-center ">
-                    <button disabled={slots.length === 0} className='btn btn-secondary text-white'>Book Appointment</button> 
+                <p>{slots.length > 0 ? <span>{slots[0]}</span> : <span className='text-red-500'>No Slot Available</span>}</p>
+                <p className=' '>{slots.length} {slots.length > 1 ? 'SPACES' : 'SPACE'} AVAILABLE</p>
+                <div class="card-actions justify-center ">
+                    <label
+                        disabled={slots.length === 0}
+                        onClick={() => setTreatment(service)}
+                        for="booking-modal" class="btn btn-secondary text-white">Book Appointment
+                    </label>
                 </div>
             </div>
         </div>
