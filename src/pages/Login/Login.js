e1265f6068dcf189ee3c28206727de62b89d463a
masud rana <masudranaacb@gmail.com>
2022-05-14 15:09:30 +0600

    added signin email password

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,17 +1,94 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
 import auth from '../../firebase.init';
+import Loading from '../sheard/Loading';
 const Login = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-    if(user){
-        console.log(user)
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    if(loading || gLoading){
+        return <Loading></Loading>
     }
+    let signinError;
+    if(error || gError){
+        signinError=<p className='text-red-500'><small>{error?.message||gError?.message}</small></p>
+    }
+    if (gUser) {
+        console.log(gUser)
+    }
+    const onSubmit = data => {
+        console.log(data)
+        signInWithEmailAndPassword(data.email,data.password)
+    };
     return (
         <div className='flex justify-center my-12'>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
                     <h2 className="text-center text-2xl font-bold">Login</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+
+                    <form onSubmit={handleSubmit(onSubmit)}>
+
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">Email</span>
+                            </label>
+                            <input
+                                type="email"
+                                placeholder="Your Email"
+                                class="input input-bordered w-full max-w-xs"
+                                {...register("email", {
+                                    required: {
+                                        value: true,
+                                        message: 'email is requird'
+                                    },
+                                    pattern: {
+                                        value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                        message: 'Type a valid email'
+                                    }
+                                })}
+                            />
+                            <label class="label">
+                                {errors.email?.type === 'required' && <span class="label-text-alt text-red-500">{errors.email.message}</span>}
+                                {errors.email?.type === 'pattern' && <span class="label-text-alt text-red-500">{errors.email.message}</span>}
+                            </label>
+                        </div>
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">Password</span>
+                            </label>
+                            <input
+                                type="password"
+                                placeholder="Your password"
+                                class="input input-bordered w-full max-w-xs"
+                                {...register("password", {
+                                    required: {
+                                        value: true,
+                                        message: 'password is requird'
+                                    },
+                                    maxLength: {
+                                        value: 6,
+                                        message: 'minimum length 6 character password'
+                                    }
+                                })}
+                            />
+                            <label class="label">
+                                {errors.password?.type === 'required' && <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+                                {errors.password?.type === 'maxLength' && <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+                            </label>
+                        </div>
+                        {signinError}
+                        <input className='btn w-full max-w-xs' type="submit" value="Login" />
+                    </form>
+
+
+
+
                     <div className="divider">OR</div>
                     <button className="btn btn-outline" onClick={() => signInWithGoogle()}>Continu White Google</button>
                 </div>
